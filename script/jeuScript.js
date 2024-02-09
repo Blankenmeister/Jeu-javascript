d5c33c3b080c8e3ab6d3d8a18f2662cf146a4e9a
Sonia <sonia.blankenmeister.simplon@gmail.com>
2024-02-09 15:16:07 +0100

    nouvelles images

--- script/jeuScript.js
+++ script/jeuScript.js
@@ -9,7 +9,7 @@ let isMouseClicked = false;
 
 // en cas de click à un endroit de l'écran
 // le carré rouge se déplace à cet endroit là
-fondJeu.addEventListener("click", (event) => {    
+fondJeu.addEventListener("click", (event) => {
     let x = event.clientX - 50;
     let y = event.clientY - 50;
     console.log("Position X : " + x + " & position Y : " + y);
@@ -25,39 +25,48 @@ const saucisse = document.querySelector('#saucisse');
 window.addEventListener('keydown', (event) => {
     const pressedkey = event.code;
     //console.log(saucisse.getBoundingClientRect());
-    
-    if (pressedkey === "Space") {
+
+    if (pressedkey === "KeyD") {
         setInterval(bougerSaucisse, 30);
         function bougerSaucisse() {
             const fondJeuRight = fondJeu.getBoundingClientRect().right;
             const saucisseRight = saucisse.getBoundingClientRect().right;
-            const personnageRight = saucisse.getBoundingClientRect().right;
+            const personnageRight = personnage.getBoundingClientRect().right;
             //const personnageLeft = saucisse.getBoundingClientRect().left;
 
-            if (saucisseRight < personnageRight) {
-                saucisse.style.left = saucisse.getBoundingClientRect().left + 10 + "px";
-            //} else saucisse.remove();
+            console.log(fondJeuRight);
+            console.log(saucisseRight);
 
-            } else if (saucisseRight < fondJeuRight) {
-                saucisse.style.left = saucisse.getBoundingClientRect().left + 10 + "px";
-            //console.log("fondJeu.getBoundingClientRect()");
+            if (saucisseRight < fondJeuRight) {
+                saucisse.style.left = saucisse.getBoundingClientRect().left + 5 + "px";
+console.log("jjjjjj");
             } else saucisse.remove();
-            
+
+            if (saucisseRight < personnageRight) {
+                saucisse.style.left = saucisse.getBoundingClientRect().left + 5 + "px";
+
+
+
+            } else saucisse.remove();
+
+        }
+
         //saucisse.style.left = saucisse.getBoundingClientRect().left + 10 + "px";
-        
+
     } //else if () {
-        
-    }
+
+
+
 })
 
 
 //Définir que la saucisse apparait à un endroit aléatoire de Y 
 
-Math.floor(Math.random()*100000000);
+Math.floor(Math.random() * 100000000);
 
 function getRandomNumber(longueurTableau) {
-    let nombreAleatoire = Math.floor(Math.random()*longueurTableau);
-    
+    let nombreAleatoire = Math.floor(Math.random() * longueurTableau);
+
     //choisir au hasard un nombre entre 0 et la longueur du tableau
     return nombreAleatoire;
 }
